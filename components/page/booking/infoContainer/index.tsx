8832ac1082315397cefbf6bf591c8069cf4f8a97
Harsh-Sharma-1 <harsh112200harsh@gmail.com>
2022-08-22 11:28:54 +0530

    booking scrollbar added

--- components/page/booking/infoContainer/index.tsx
+++ components/page/booking/infoContainer/index.tsx
@@ -6,30 +6,32 @@ const InfoContainer = () => {
     return (
         <div className={styles.container}>
             <Corousal />
-            <InfoText
-                heading='Overview'
-                paragraph={`We believe that redesigning the structure of learning
+            <div className={styles.infoContainer}>
+                <InfoText
+                    heading='Overview'
+                    paragraph={`We believe that redesigning the structure of learning
                  through travel is the need of the hour and we are here
                   to disrupt the taboo of taking GAP Days/Week/Month and Year in India.`}
-            />
-            <InfoText
-                heading='Itinerary along with inclusions and exclusions'
-                paragraph={`We believe that redesigning the structure of learning
+                />
+                <InfoText
+                    heading='Itinerary along with inclusions and exclusions'
+                    paragraph={`We believe that redesigning the structure of learning
                  through travel is the need of the hour and we are here
                   to disrupt the taboo of taking GAP Days/Week/Month and Year in India.`}
-            />
-            <InfoText
-                heading='Terms and Conditions'
-                paragraph={`We believe that redesigning the structure of learning
+                />
+                <InfoText
+                    heading='Terms and Conditions'
+                    paragraph={`We believe that redesigning the structure of learning
                  through travel is the need of the hour and we are here
                   to disrupt the taboo of taking GAP Days/Week/Month and Year in India.`}
-            />
-            <InfoText
-                heading='Cancellation Policy'
-                paragraph={`We believe that redesigning the structure of learning
+                />
+                <InfoText
+                    heading='Cancellation Policy'
+                    paragraph={`We believe that redesigning the structure of learning
                  through travel is the need of the hour and we are here
                   to disrupt the taboo of taking GAP Days/Week/Month and Year in India.`}
-            />
+                />
+            </div>
         </div>
     );
 };
